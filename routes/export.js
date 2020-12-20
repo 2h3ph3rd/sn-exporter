9b5235d94814027f1dea9e29536b656bf20ba1a3
Francesco Pastore <francesco0599@hotmail.it>
2020-12-20 16:45:07 +0100

    Working on exporting notes to pdf

--- routes/export.js
+++ routes/export.js
@@ -6,28 +6,30 @@ router.get('/:file_type/:item_uuid', function (req, res, next) {
   file_type = req.params.file_type
   item_uuid = req.params.item_uuid
 
-  data = store.notes.get(item_uuid)
+  text = store.notes.get(item_uuid)
 
-  if (data == null) {
+  if (text == null) {
     res.send('Note not found')
     return
   }
 
+  data = ''
   switch (file_type) {
     case 'pdf':
       res.attachment('Note.pdf')
-      res.type('txt')
-      break
+      res.type('pdf')
+      data = text
     case 'txt':
       res.attachment('Note.txt')
       res.type('txt')
+      data = text
       break
     default:
       res.send('File type not supported')
       return
   }
 
-  res.send(text)
+  res.send(data)
 })
 
 module.exports = router
--- store/notes.js
+++ store/notes.js
@@ -1,4 +1,32 @@
-store = {}
+store = {
+  '4dc03b2b-ad6f-4e32-8803-c1ddc6f577f7': {
+    title: 'Note 36',
+    text:
+      '### Test\n\n## Test\n\n# Test\n\n* a \n* a\n* a\n* a\n\n\n\n1. a\n2. a\n3. a\n4. a\n\n\n',
+    parsedText:
+      '# Note 36\n' +
+      '\n' +
+      '### Test\n' +
+      '\n' +
+      '## Test\n' +
+      '\n' +
+      '# Test\n' +
+      '\n' +
+      '* a \n' +
+      '* a\n' +
+      '* a\n' +
+      '* a\n' +
+      '\n' +
+      '\n' +
+      '\n' +
+      '1. a\n' +
+      '2. a\n' +
+      '3. a\n' +
+      '4. a\n' +
+      '\n' +
+      '\n',
+  },
+}
 
 module.exports = {
   save(note) {
