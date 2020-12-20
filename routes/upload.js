b95fb71db9eb21e893fa3b76bf95df8d1416f86f
Francesco Pastore <francesco0599@hotmail.it>
2020-12-20 15:44:12 +0100

    Updated upload endpoint to use store

--- routes/upload.js
+++ routes/upload.js
@@ -1,10 +1,16 @@
 var express = require('express')
 var router = express.Router()
+var store = require('../store')
 
 router.post('/', function (req, res, next) {
+  // read note data
   title = req.body.items[0].content.title
   text = req.body.items[0].content.text
-  req.session.notes[req.query.item_uuid] = '# ' + title + '\n\n' + text
+  item_uuid = req.body.items[0].uuid
+
+  // save note for export
+  store.notes[item_uuid] = req.body.items[0]
+  store.notes[item_uuid].parsedText = '# ' + title + '\n\n' + text
 
   res.send('Note saved')
 })
