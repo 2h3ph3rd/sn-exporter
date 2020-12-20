8e6fe44b09d5e373f58d2797c268aa69d7199c9a
Francesco Pastore <francesco0599@hotmail.it>
2020-12-20 15:05:20 +0100

    Added upload enpoint

--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,12 @@
 var express = require('express')
-const { reset } = require('nodemon')
 var router = express.Router()
 
-router.get('/', function (req, res, next) {
-  res.send('Note saved!')
+router.post('/', function (req, res, next) {
+  title = req.body.items[0].content.title
+  text = req.body.items[0].content.text
+  req.session.notes[req.query.item_uuid] = '# ' + title + '\n\n' + text
+
+  res.send('Note saved')
 })
 
 module.exports = router
