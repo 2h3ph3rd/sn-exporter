27270b359eb2a6a354883cdc4ec3aedc1fb7c8db
Francesco Pastore <francesco0599@hotmail.it>
2020-12-21 09:11:03 +0100

    Fixed file name in markdown toPdf utility

--- utilities/markdown.js
+++ utilities/markdown.js
@@ -3,17 +3,24 @@ var fs = require('fs')
 
 module.exports = {
   toPdf(item_uuid, text) {
-    filename = item_uuid
-    markdown_filename = filename + '.md'
-    pdf_filename = filename + '.pdf'
+    base_path = './tmp/' + item_uuid
+    markdown_file_path = base_path + '/note.md'
+    pdf_file_path = base_path + '/note.pdf'
 
-    fs.writeFileSync(markdown_filename, text)
+    // create folder for conversion
+    execSync('mkdir ' + base_path)
 
-    execSync('md-to-pdf ' + markdown_filename)
-    pdf = fs.readFileSync(pdf_filename)
+    // write text to file
+    fs.writeFileSync(markdown_file_path, text)
 
-    fs.unlinkSync(markdown_filename)
-    fs.unlinkSync(pdf_filename)
+    // convert file to pdf
+    execSync('md-to-pdf ' + markdown_file_path)
+
+    // read file converted
+    pdf = fs.readFileSync(pdf_file_path)
+
+    // remove tmp files stored in memory
+    execSync('rm -rf ' + base_path)
 
     return pdf
   },
