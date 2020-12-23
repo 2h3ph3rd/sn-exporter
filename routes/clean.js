78cd81af2397666a3746b80a9887957517a387bb
Francesco Pastore <francesco0599@hotmail.it>
2020-12-23 12:02:52 +0100

    Added clean route

---
+++ routes/clean.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var router = express.Router()
+var store = require('../store')
+
+router.get('/', function (req, res, next) {
+  store.notes.clean()
+  res.send('Cleaned done!')
+})
+
+module.exports = router
