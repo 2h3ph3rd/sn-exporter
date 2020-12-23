6b377eb3a61726895cc538be24d358d1b24526d7
Francesco Pastore <francesco0599@hotmail.it>
2020-12-23 09:55:36 +0100

    Fixed not found handling in delete endpoint

--- routes/delete.js
+++ routes/delete.js
@@ -2,15 +2,16 @@ var express = require('express')
 var router = express.Router()
 var store = require('../store')
 
-router.get('/:item_uuid', function (req, res, next) {
-  item_uuid = req.query.item_uuid
+router.post('/:item_uuid', function (req, res, next) {
+  item_uuid = req.params.item_uuid
 
-  if (store.notes.present(item_uuid)) {
-    store.notes.delete(item_uuid)
-    res.send('Note deleted')
-  } else {
+  if (!store.notes.present(item_uuid)) {
     res.status(404).send('Note not found')
+    return
   }
+
+  store.notes.delete(item_uuid)
+  res.send('Note deleted')
 })
 
 module.exports = router
